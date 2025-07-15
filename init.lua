49fdd64efee2b23f7fd3d4520f330e93a07b00dd
Sebastian <sebastian.neumair@gmail.com>
2025-07-15 16:51:33 +0200

    add treesitter setup

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ require('lazy').setup({
     require 'plugins.neotree',
     require 'plugins.colortheme',
     require 'plugins.bufferline',
-    require 'plugins.lualine'
+    require 'plugins.lualine',
+    require 'plugins.treesitter'
 })
 
