7e4e11ff1dc4b9cfeb53dc294ba592a1f4907c40
Sebastian <sebastian.neumair@gmail.com>
2025-07-15 16:32:07 +0200

    setup bufferline

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ rtp:prepend(lazypath)
 
 require('lazy').setup({
     require 'plugins.neotree',
-    require 'plugins.colortheme'
+    require 'plugins.colortheme',
+    require 'plugins.bufferline'
 })
 
