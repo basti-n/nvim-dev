ee555006ce620d9cc101497df78ca6c6e3850b77
Sebastian <sebastian.neumair@gmail.com>
2025-07-15 21:14:34 +0200

    add telescope setup

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ require('lazy').setup({
     require 'plugins.colortheme',
     require 'plugins.bufferline',
     require 'plugins.lualine',
-    require 'plugins.treesitter'
+    require 'plugins.treesitter',
+    require 'plugins.telescope'
 })
 
