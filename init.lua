16609870b78f2912410d7bed1ab951e1e413c1fe
Sebastian <sebastian.neumair@gmail.com>
2025-07-15 16:42:53 +0200

    add lualine setup

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
     require 'plugins.neotree',
     require 'plugins.colortheme',
-    require 'plugins.bufferline'
+    require 'plugins.bufferline',
+    require 'plugins.lualine'
 })
 
