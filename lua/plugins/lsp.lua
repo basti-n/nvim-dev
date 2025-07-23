2fbd7dab171ec2324ad54ad1ee3ae78f5eb424b7
Sebastian <sebastian.neumair@redmedial.de>
2025-07-23 10:17:18 +0200

    add lsp.buf.hover keymap ("K")

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,6 +38,9 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
+        -- Hover documentation
+        map('K', vim.lsp.buf.hover, 'Show hover documentation')
+
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
