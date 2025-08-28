d00394d92d66b3aa5dc5e3c2984ccbf216187902
Sebastian <sebastian.neumair@redmedial.de>
2025-08-28 15:33:34 +0200

    add copilot chat

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -32,6 +32,7 @@ return { -- Highlight, edit, and navigate code
       'css',
       'html',
       'angular',
+      'diff',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
